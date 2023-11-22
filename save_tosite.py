86f6bdff1d80b598d135be1f705f18b496752b03
shivashah1 <shivashah1@outlook.com>
2023-11-22 13:25:26 +0100

    beep

--- save_tosite.py
+++ save_tosite.py
@@ -54,16 +54,21 @@ output_html_path = 'docs/html_test.html'
 converted_html_path = convert_notebook(notebook_path, output_html_path, None)
 print(f"Notebook converted to HTML: {converted_html_path}")
 
-
-# %%
+#%%
 import nbformat
 from nbconvert import HTMLExporter
-from nbconvert.preprocessors import ExecutePreprocessor, ExtractOutputPreprocessor, TagRemovePreprocessor
+from nbconvert.preprocessors import ExecutePreprocessor, ExtractOutputPreprocessor
 from traitlets.config import Config
 import os
 import shutil
 
-def convert_notebook_hide_code(notebook_path, output_html_path, additional_files=None, hide_tag='hide_code'):
+def filter_code_cells(cell):
+    """
+    Filter function to exclude code cells.
+    """
+    return cell.cell_type == 'code'
+
+def convert_notebook_hide_code(notebook_path, output_html_path, additional_files=None):
     """
     Converts a Jupyter notebook to an HTML file suitable for GitHub pages, hiding code cells.
 
@@ -71,7 +76,6 @@ def convert_notebook_hide_code(notebook_path, output_html_path, additional_files
     notebook_path (str): Path to the Jupyter notebook file.
     output_html_path (str): Path where the HTML file will be saved.
     additional_files (list of str, optional): List of paths to additional files required for the notebook.
-    hide_tag (str): Tag used to mark code cells for hiding.
     """
     # Copy additional files to the current working directory, if provided
     if additional_files:
@@ -86,17 +90,13 @@ def convert_notebook_hide_code(notebook_path, output_html_path, additional_files
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
     ep.preprocess(nb, {'metadata': {'path': '.'}})
 
-    # Configure preprocessors
+    # Configure HTMLExporter
     c = Config()
-    c.TagRemovePreprocessor.enabled = True
-    c.TagRemovePreprocessor.remove_cell_tags = (hide_tag,)
-    c.HTMLExporter.preprocessors = [ExtractOutputPreprocessor(), TagRemovePreprocessor(config=c)]
+    c.HTMLExporter.preprocessors = [ExtractOutputPreprocessor()]
+    c.TemplateExporter.exclude_input_cells = True  # Exclude code cells
 
     # Convert the notebook to HTML
     html_exporter = HTMLExporter(config=c)
-    html_exporter.template_name = 'classic'
-
-    # Export the notebook to HTML
     (body, resources) = html_exporter.from_notebook_node(nb, resources=c)
     
     # Write the HTML to a file
@@ -107,11 +107,11 @@ def convert_notebook_hide_code(notebook_path, output_html_path, additional_files
 
 # Example usage of the function
 notebook_path = 'report_generator.ipynb'
-output_html_path = 'docs/html_test2.html'
+output_html_path = 'docs/html_test3.html'
+# additional_files = ['path_to_additional_file1', 'path_to_additional_file2'] # Optional
 
 # Convert the notebook
 converted_html_path = convert_notebook_hide_code(notebook_path, output_html_path, None)
 print(f"Notebook converted to HTML: {converted_html_path}")
-
 
 # %%
