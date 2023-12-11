eb4de5fe022c4a524f79c450d0d5126194d1fd96
Shiva <35211454+gl0bsec@users.noreply.github.com>
2023-12-11 17:11:44 +0100

    fixed test site

--- save_tosite.py
+++ save_tosite.py
@@ -47,14 +47,14 @@ def convert_notebook(notebook_path, output_html_path, additional_files=None):
 
 # Example usage of the function
 notebook_path = 'report_generator.ipynb'
-output_html_path = 'docs/israel_test1.html'
+output_html_path = 'docs/mea_simple.html'
 # additional_files = ['path_to_additional_file1', 'path_to_additional_file2'] # Optional
 
 # Convert the notebook
 converted_html_path = convert_notebook(notebook_path, output_html_path, None)
 print(f"Notebook converted to HTML: {converted_html_path}")
 
-#%%
+`2#%%
 import nbformat
 from nbconvert import HTMLExporter
 from nbconvert.preprocessors import ExecutePreprocessor, ExtractOutputPreprocessor
